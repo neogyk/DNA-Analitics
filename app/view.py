9d55e079e8e0d3bb872c4b26337858d7df266400
ledidukh1212 <ledidukh@gmail.com>
2016-04-06 19:58:40 +0300

    D3.js Diagram builded

--- app/view.py
+++ app/view.py
@@ -1,15 +1,19 @@
 from app import app
-from flask import render_template,jsonify
+from flask import render_template,jsonify,request
 import pandas as pd
 @app.route('/')
-@app.route('/index')
 def startPage():
     print "Plus"
     return render_template('start.html')
 @app.route('/getDataCSV')
-def getDataCSV():
-    df = pd.read_csv('templates/dataCSV/24_03-17_03.csv')
-    dataJson = df.to_json()
+def getData():
     print 'Radar'
+    df = pd.read_csv("C:/Users/l.didukh/Desktop/Flask/Info Flask/app/templates/dataCSV/24_03-17_03.csv")
+    name_statistics= df.NAME.value_counts()
+    data = [{'name':i,'value':name_statistics[i]} for i in name_statistics.index]
+    dataJson = pd.DataFrame(data,columns=['name','value']).to_json()
+  
+    #result=pd.DataFrame(data,columns=['name','value'])
+    #print result
     return dataJson
    
