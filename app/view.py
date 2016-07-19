9a12e61b47a76d5e5ae77104526c871b7b2f48e5
Igor Podmokov <i.podmokov@fly.local>
2016-07-19 18:03:22 +0300

    Fixed several mistaces

--- app/view.py
+++ app/view.py
@@ -1,7 +1,7 @@
 from app import app
 from flask import render_template,jsonify,request
 import pandas as pd
-
+import numpy as np
 
 def diff(list_1,list_2):
     d_list = []
@@ -23,7 +23,21 @@ def startPage():
 @app.route('/getDataCSV')
 def get_Data():
     print 'Radar'
+    
     df = pd.read_csv("P:/TANNER/PREP/MANAGE/DataBase_Ftrack/Task Statistics/TotalStatistics/17.06.2016/REEL_04.csv")
-    dataJson = df.to_json()
-    return dataJson
+    #_df = df[['Name','Status','Duration']]
+    
+    #_df['Name'].values
+    ##Return All for Animation Data:
+    animationData = df[df.Name=="Rendering"][['Name','Status','Duration']]
+    #taskNames = np.unique(_df['Name'].values).tolist()
+    dInform = []
+    statusLists = np.unique(animationData['Status'].values).tolist()
+    for status in statusLists:
+        dInform.append({"Name":status,"Duration":animationData[animationData.Status==status]["Duration"].sum()})
+    print(dInform)
+    #print(np.unique(_df['Duration'].values).tolist())
+    animationData
+    dataJson = animationData.to_json()
+    return jsonify(dInform)
 
